eeb8a59ff1b265a824c61aac666769b63eb714c9
Eduardo Carneiro <eduardo.carneiro2907@gmail.com>
2022-07-04 17:09:09 -0300

    Add board with info about the game (currently static)
    Also added word length filter (5-12)

--- main.rb
+++ main.rb
@@ -3,6 +3,27 @@ class Game
   def initialize
     @word_master = WordMaster.new
     @word_guesser = WordGuesser.new
+    @wrong_letters = Array.new(6) { ' ' }
+    @guess = Array.new(@word_master.word_length) { '_' }
+  end
+
+  def run
+    print_board
+  end
+
+  private
+
+  def print_board
+    puts <<~HEREDOC
+       =================================
+      ||                               ||
+      || Lifes: #{@word_guesser.lifes.join(' ')}            ||
+      || Wrong Letters: #{@wrong_letters.join(' ')}    ||
+      ||                               ||
+      || Word: #{@guess.join(' ').ljust(23)} ||
+      ||                               ||
+       =================================
+    HEREDOC
   end
 end
 
@@ -12,19 +33,32 @@ class WordMaster
     @word = pick_random_word
   end
 
+  def word_length
+    @word.length
+  end
+
   private
 
   def pick_random_word
     chosen_word = nil
-    File.foreach('dictionary.txt').each_with_index do |word, number|
-      chosen_word = word if rand < (1.0 / (number + 1))
+    begin
+      File.foreach('dictionary.txt').each_with_index do |word, number|
+        chosen_word = word if (rand < (1.0 / (number + 1))) && (6..13).include?(word.length)
+      end
+      chosen_word.chomp
+    rescue NoMethodError
+      pick_random_word
     end
-    chosen_word.chomp
   end
 end
 
 # The player who tries to guess the correct letters
 class WordGuesser
+  attr_accessor :lifes
+
+  def initialize
+    @lifes = ['♥', '♥', '♥', '♥', '♥', '♥']
+  end
 end
 
-Game.new
+Game.new.run
